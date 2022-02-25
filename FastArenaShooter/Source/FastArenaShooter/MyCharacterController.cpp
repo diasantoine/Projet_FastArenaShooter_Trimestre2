ecb35a8be6d0e8f41b8d968c143052bf227d9224
diasantoine <61905755+diasantoine@users.noreply.github.com>
2022-02-25 17:14:09 +0100

    Still trying to make it work (bunny hop)

--- FastArenaShooter/Source/FastArenaShooter/MyCharacterController.cpp
+++ FastArenaShooter/Source/FastArenaShooter/MyCharacterController.cpp
@@ -95,22 +95,22 @@ void AMyCharacterController::PitchRotation(float _value)
 void AMyCharacterController::YawRotation(float _value)
 {
 	APawn::AddControllerYawInput(_value);
-	//Calculate current speed
-	FVector horizontalMovement = GetCharacterMovement()->Velocity;
-	horizontalMovement.Z = 0.0f;
-	float speed = horizontalMovement.Size();
-	
-	//Get the rotation of pawn
-	FRotator rotation = GetActorRotation();
-	FVector rotationVec = rotation.Vector();
-	
-	//Apply speed to rotation vector
-	rotationVec.X *= speed;
-	rotationVec.Y *= speed;
-	
-	//Set new movement velocity
-	GetCharacterMovement()->Velocity.X = rotationVec.X;
-	GetCharacterMovement()->Velocity.Y = rotationVec.Y;
+	// //Calculate current speed
+	// FVector horizontalMovement = GetCharacterMovement()->Velocity;
+	// horizontalMovement.Z = 0.0f;
+	// float speed = horizontalMovement.Size();
+	//
+	// //Get the rotation of pawn
+	// FRotator rotation = GetActorRotation();
+	// FVector rotationVec = rotation.Vector();
+	//
+	// //Apply speed to rotation vector
+	// rotationVec.X *= speed;
+	// //rotationVec.Y *= speed;
+	//
+	// //Set new movement velocity
+	// GetCharacterMovement()->Velocity.X = rotationVec.X;
+	// GetCharacterMovement()->Velocity.Y = rotationVec.Y;
 }
 
 
